afaca4fe507f6444a3bce2bda806ea57ea8a5a55
hvckxm <barsiklegend@gmail.com>
2022-02-08 15:34:01 +0400

    Created models

--- web_shop/shop/models.py
+++ web_shop/shop/models.py
@@ -2,15 +2,26 @@ from django.db import models
 
 
 # Create your models here.
-class Product(models.Model):
-    name = models.CharField(max_length=128)
-
-    def __str__(self):
-        return self.name
-
-
 class Category(models.Model):
     name = models.CharField(max_length=128)
 
     def __str__(self):
         return self.name
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=128)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
+class Order(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    address = models.TextField()
+    tracking_number = models.TextField()
+    status = models.IntegerField()
+
+    def __str__(self):
+        return self.status
